a95b52387ee67223a052bc136b3816a12222e8da
iamdeepta <47137605+iamdeepta@users.noreply.github.com>
2022-09-03 12:13:33 +0600

    set up post input field state and designed post button

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Post.scss";
 import { FaVideo } from "react-icons/fa";
 import { BsImages } from "react-icons/bs";
@@ -6,13 +6,18 @@ import { GoSmiley } from "react-icons/go";
 import PostItem from "./PostItem";
 
 const Post = () => {
+  const [text, setText] = useState("");
   return (
     <>
       <div className="post">
         <div className="post_input">
           <div className="post_input_up">
             <img src="https://picsum.photos/200/300?random=1" alt="profile" />
-            <input type="text" placeholder="What's on your mind, Deepta?" />
+            <input
+              type="text"
+              placeholder="What's on your mind, Deepta?"
+              onChange={(e) => setText(e.target.value)}
+            />
           </div>
           <div className="post_input_down">
             <div className="post_input_down_options">
@@ -27,6 +32,9 @@ const Post = () => {
               <GoSmiley className="post_input_down_options_icon feeling" />
               <span>Feeling/activity</span>
             </div>
+          </div>
+          <div className="post_input_button">
+            <button className="btn_post">Post</button>
           </div>
         </div>
 
